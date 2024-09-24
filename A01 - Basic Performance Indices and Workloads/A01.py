b193b33e469ef0b3cfea20ca4b00447b7acda1ad
Andrea Pesciotti <10715428@polimi.it>
2024-09-24 20:11:50 +0200

    A01 almost done

--- A01 - Basic Performance Indices and Workloads/A01.py
+++ A01 - Basic Performance Indices and Workloads/A01.py
@@ -1,22 +1,59 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Caricare i dati dai file CSV
-data_logger1 = np.loadtxt('Logger1.csv', delimiter=',')
-data_logger2 = np.loadtxt('Logger2.csv', delimiter=',')
-
-# Verificare i dati caricati
-print("Logger1 data:", data_logger1)
-print("Logger2 data:", data_logger2)
-
-# Total Time
-T = data_logger2[-1] - data_logger1[0]
-print("Total time:", T)
+# Load data from the files
+A_T = np.loadtxt('Logger1.csv', delimiter=',')
+C_T = np.loadtxt('Logger2.csv', delimiter=',')
 
 # Number of cars
-num_cars = len(data_logger1)
-print("Number of cars:", num_cars)
+C = len(A_T)
 
-# Arrival rate
-A_T = num_cars/T
-print("Average time per car:", A_T)
+# Total Time
+T = C_T[-1]
+
+# Arrival Rate
+lam = C / T
+
+# Throguhput
+X = C / T
+
+# Inter-Arrival Time
+inter_arrival_times = np.diff(A_T)
+# Average Inter-Arrival Time
+average_inter_arrival_time_v1 = 1 / lam
+average_inter_arrival_time_v2 = np.mean(inter_arrival_times)
+
+# Response Times
+r_i = C_T - A_T
+# Average Response Time
+R = np.mean(r_i)
+
+# Number of Jobs with Little's Law
+N = X * R
+
+# Service Times
+service_times = np.zeros(C)
+service_times[0] = C_T[0] - A_T[0]
+for i in range (1, C):
+    service_times[i] = C_T[i] - max(A_T[i], C_T[i - 1])
+
+# Average Service Time
+S = np.mean(service_times)
+
+# Busy Time
+B = S * C
+
+# Utilization Law
+U = B / T
+
+print(f"Number of cars: {C} cars")
+print(f"Total time: {T} min")
+print(f"Average time per car: {lam} cars/min")
+print(f"Throughput: {X} cars/min")
+print(f"Average Inter-Arrival Time v1: {average_inter_arrival_time_v1} min")
+print(f"Average Inter-Arrival Time v2: {average_inter_arrival_time_v2} min")
+print(f"Average Service Time: {S} min")
+print(f"Busy Time: {B} min")
+print(f"Utilization: {U} min")
+print(f"Average Response Time: {R} min")
+print(f"Number of jobs: {N} jobs")
--- L02 - Basic Performance Indices/LogAnalysis.py
+++ L02 - Basic Performance Indices/LogAnalysis.py
@@ -1,88 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# loads the traces
-intrace = np.loadtxt("LogFile.csv",delimiter=";")
-inrows = intrace.shape[0]
 
-# A(T) is created summing the inter-arrival times
-A_T = np.zeros((inrows,1))
-A_T[:,0] = np.cumsum(intrace[:,0])  # np.cumsum() sums the element of intrace
+trace = np.loadtxt('LogFile.csv', delimiter=";")
+C = len(trace)
+print("Completion: ", C)
 
-# C(T) is created using either the completion of the previous job, or the arrival time
-C_T = np.zeros((inrows,1))
-C_T[0,0] = intrace[0,1] + A_T[0,0]
-for i in range(1, inrows):
-    C_T[i,0] = max(C_T[i-1,0], A_T[i,0]) + intrace[i,1]
+A_T = np.cumsum(trace[:,0]).reshape(-1,1)
+print(A_T)
 
-## plots the arrival and departure curves    
-plt.plot(A_T[0:250,0], np.r_[0:250]);
-plt.plot(C_T[0:250,0], np.r_[0:250]);
+# Inizializza C_T come un vettore colonna
+C_T = np.zeros((C, 1))
+
+# Calcola il primo tempo di completamento
+C_T[0, 0] = trace[0, 1] + A_T[0, 0]
+
+# Usa un ciclo for per calcolare i tempi di completamento successivi
+for i in range(1, C):
+    C_T[i, 0] = max(C_T[i - 1, 0], A_T[i, 0]) + trace[i, 1]
+
+T = C_T[-1,0]
+print("Time: ", T)
+
+# Visualizza il risultato
+print(C_T)
+
+
+plt.plot(A_T[:,0], np.r_[0:C])
+plt.plot(C_T[:,0], np.r_[0:C])
 plt.show()
 
-## defines the total time, as the arrival time of the last job.
-## many alternative definitions are possible
-T = A_T[-1,0]
-## Computes the basic performance indices
-B = np.sum(intrace[:,1]) ## since here the second column of "intrace" are service times
-U = B / T
-X = inrows / T ## since the number of rows of "intrace" represents the completed jobs
-S = U / X
 
-## compute the response time of the jobs in r_i
+X = C / T
+lam = C / T
+
+B = np.sum(trace[:,1])
+U = B / T
+S = B / C
+
+print("Throughput: ", X)
+print("Lambda: ", lam)
+print("Busy Time: ", B)
+print("Utilization: ", U)
+print("Service time: ", S)
+
 r_i = C_T - A_T
 R = np.mean(r_i)
-## The number of jobs is computed using Little's law
-N = X * R 
+N = X * R
 
-## compute the P(R>15) as the fraction of jobs where r_i > 15, given by np.sum(r_i > 15)
-pRg15 = np.sum(r_i > 15) / inrows
-
-## prints the results
-print("T = ", T)
-print("B = ", B)
-print("U = ", U)
-print("X = ", X)
-print("S = ", S)
-print("R = ", R)
-print("N = ", N)
-print("p(R > 15) = ", pRg15)
-
-## creates in AC_T a 2 * inrows array where the first column represents the t_i
-## in slide 20 of lesson 2, and in the second column +1 for arrival, and -1 for service
-AC_T = np.block([[A_T, np.ones((inrows,1))],[C_T, -np.ones((inrows,1))]])
-AC_T = AC_T[AC_T[:, 0].argsort()]
-## by summing the second column, computes the number of jobs in each time segment
-AC_T[:,1] = np.cumsum(AC_T[:,1])
-
-## dTN_T is a matrix that contains in the first row the time spent with the number
-## specified in the second column
-dTN_T = (np.c_[[AC_T[1:-1,0] - AC_T[0:-2,0], AC_T[:-2,1]]]).T
-
-## in this case, too increase the precision a little bit, T is defined as the sum of times
-## in the first column of dTN_T, which due to its definition can be computed more
-## efficiently as the difference between the time of the last departure
-## and the time of the first arrival
-TforP = AC_T[-1,0] - AC_T[0,0]
-
-## Plots the queue length distribution
-PN = [0]*26
-PNL = [0]*26
-for i in range(0,26):
-    PNL[i] = i
-    PN[i] = np.sum(dTN_T[dTN_T[:,1] == i,0]) / TforP
-    
-plt.bar(PNL, PN)    
-plt.show()
-
-## Plots the response time distribution
-PR  = [0]*101
-PRT = [0]*101
-for i in range(0,101):
-    t = i / 10
-    PRT[i] = t
-    PR[i] = sum(r_i < t) / inrows
-    
-plt.plot(PRT, PR);
-plt.show()
-    
+print("Response Time: ", R)
+print("Number of Jobs: ", N)
