70ed917e13e4799eb755224f9aee7ec014da1146
Andrea Pesciotti <10715428@polimi.it>
2024-09-27 22:30:41 +0200

    working on distribution

--- A01 - Basic Performance Indices and Workloads/A01.py
+++ A01 - Basic Performance Indices and Workloads/A01.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Load data from the files
 A_T = np.loadtxt('Logger1.csv', delimiter=',')
 C_T = np.loadtxt('Logger2.csv', delimiter=',')
@@ -9,6 +10,7 @@ C_T = np.loadtxt('Logger2.csv', delimiter=',')
 C = len(A_T)
 
 # Total Time
+# it is possible to pick it many other different ways
 T = C_T[-1]
 
 # Arrival Rate
@@ -20,8 +22,7 @@ X = C / T
 # Inter-Arrival Time
 inter_arrival_times = np.diff(A_T)
 # Average Inter-Arrival Time
-average_inter_arrival_time_v1 = 1 / lam
-average_inter_arrival_time_v2 = np.mean(inter_arrival_times)
+average_inter_arrival_time = 1 / lam
 
 # Response Times
 r_i = C_T - A_T
@@ -32,13 +33,11 @@ R = np.mean(r_i)
 N = X * R
 
 # Service Times
-service_times = np.zeros(C)
-service_times[0] = C_T[0] - A_T[0]
-for i in range (1, C):
-    service_times[i] = C_T[i] - max(A_T[i], C_T[i - 1])
+s_i = C_T - np.maximum(A_T, np.roll(C_T, 1))
+s_i[0] = C_T[0] - A_T[0]
 
 # Average Service Time
-S = np.mean(service_times)
+S = np.mean(s_i)
 
 # Busy Time
 B = S * C
@@ -46,14 +45,62 @@ B = S * C
 # Utilization Law
 U = B / T
 
+# Definire l'intervallo e la granularità per il tempo di risposta
+tau_values = np.arange(1, 41, 1)  # Da 1 a 40 minuti con passo di 1 minuto
+response_distribution = []
+
+for tau in tau_values:
+    prob = np.sum(r_i < tau) / C
+    response_distribution.append(prob)
+
+# Grafico della distribuzione del tempo di risposta
+plt.plot(tau_values, response_distribution, label="Response Time Distribution")
+plt.title('Response Time Distribution')
+plt.xlabel('Response Time (minutes)')
+plt.ylabel('Cumulative Probability')
+plt.grid(True)
+plt.show()
+
+# Istogramma del tempo di risposta
+plt.hist(r_i, bins=np.arange(1, 41, 1), density=True, alpha=0.75, edgecolor='black')
+plt.title('Response Time Histogram')
+plt.xlabel('Response Time (minutes)')
+plt.ylabel('Probability Density')
+plt.grid(True)
+plt.show()
+
+# Definire l'intervallo e la granularità per il tempo di servizio
+tau_service_values = np.arange(0.1, 5.1, 0.1)  # Da 0.1 a 5 minuti con passo di 0.1 minuti
+service_distribution = []
+
+for tau in tau_service_values:
+    prob = np.sum(s_i < tau) / C
+    service_distribution.append(prob)
+
+# Grafico della distribuzione del tempo di servizio
+plt.plot(tau_service_values, service_distribution, label="Service Time Distribution")
+plt.title('Service Time Distribution')
+plt.xlabel('Service Time (minutes)')
+plt.ylabel('Cumulative Probability')
+plt.grid(True)
+plt.show()
+
+# Istogramma del tempo di servizio
+plt.hist(s_i, bins=np.arange(0.1, 5.1, 0.1), density=True, alpha=0.75, edgecolor='black')
+plt.title('Service Time Histogram')
+plt.xlabel('Service Time (minutes)')
+plt.ylabel('Probability Density')
+plt.grid(True)
+plt.show()
+
+
 print(f"Number of cars: {C} cars")
 print(f"Total time: {T} min")
-print(f"Average time per car: {lam} cars/min")
+print(f"Arrival Rate: {lam} cars/min")
 print(f"Throughput: {X} cars/min")
-print(f"Average Inter-Arrival Time v1: {average_inter_arrival_time_v1} min")
-print(f"Average Inter-Arrival Time v2: {average_inter_arrival_time_v2} min")
+print(f"Average Inter-Arrival Time: {average_inter_arrival_time} min")
 print(f"Average Service Time: {S} min")
 print(f"Busy Time: {B} min")
-print(f"Utilization: {U} min")
+print(f"Utilization: {U * 100} %")
 print(f"Average Response Time: {R} min")
 print(f"Number of jobs: {N} jobs")
---
+++ A02 - Basic Performance Evaluation/A02 - Basic Performance Evaluation.md
@@ -0,0 +1,16 @@
+# Basic Performance Evaluation
+___
+The enclosed files [Logger1.csv](Logger1.csv) and [Logger2.csv](Logger2.csv) are the same as the two columns in
+Example 1: they represent respectively the inter-arrival time between two requests to a server, and
+the service time required by each request. The value is expressed in minutes.
+---
+Consider an increase in the workload, that reduces the inter-arrival time of a fraction 𝑎, that is:
+𝑎′
+𝑖 = 𝛼 ∙ 𝑎𝑖
+- Which is the maximum arrival rate [expressed in jobs per minutes] that can be handled with an
+average response time of 20 minutes?
+Consider now a fraction 𝑎 that produces an arrival rate of 1.2 jobs per minute.
+- Which should be the fraction 𝛽 at which we must reduce the service time, i.e.
+𝑠′
+𝑖 = 𝛽 ∙ 𝑠𝑖
+so that the average response time is less than 15 minutes?
