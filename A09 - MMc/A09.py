c0a0234085c40ae90666a5fd495ceb42d0ede5ef
Andrea Pesciotti <10715428@polimi.it>
2024-11-27 21:37:18 +0100

    A09 done

--- A09 - MMc/A09.py
+++ A09 - MMc/A09.py
@@ -1,77 +1,160 @@
+import math
 import numpy as np
 
-# Initial Configuration M/M/1
-print("Initial Configuration")
-
-lam = 1 / 2
 D = 1.6
-rho = lam * D
 
-U = rho
-print("Utilization of the system (U):", rho)
 
-p2 = (1 - rho) * (rho ** 2)
-print("Probability of having exactly 2 jobs in the system:", p2)
+# Utility Functions
 
-pNlt5 = sum((1 - rho) * (rho ** i) for i in range(5))
-print("Probability of having less than 5 jobs in the system:", pNlt5)
+def calculate_probabilities(lam, c):
+    rho = lam * D / c
+    p0_inv = sum([(c * rho) ** n / math.factorial(n) for n in range(c)]) + ((c * rho) ** c) / (
+            math.factorial(c) * (1 - rho))
+    p0 = 1 / p0_inv
+    return rho, p0
 
-N = rho / (1 - rho)
-Nq = N - U
-print("Average queue length: ", Nq)
 
-R = D / (1 - rho)
-print("Average response time (R):", R)
+# Configurations
 
-pRtg2 = np.exp(-2 / R)
-print("Probability that the response time is greater than 2:", pRtg2)
+def initial_configuration(lam):
+    print("Initial Configuration")
+    rho = lam * D
 
-p95 = -R * np.log(0.05)
-print("Percentile 95th:", p95)
+    # Utilization
+    U = rho
+    print("Utilization of the system (U):", U)
 
-print("")
-# Second Configuration M/M/2
-print("Second Configuration")
+    # Probability Calculations
+    p2 = (1 - rho) * (rho ** 2)
+    print("Probability of having exactly 2 jobs in the system:", p2)
 
-lam = 1
-rho = lam * D / 2
+    pNlt5 = sum((1 - rho) * (rho ** i) for i in range(5))
+    print("Probability of having less than 5 jobs in the system:", pNlt5)
 
-Ub = rho
-U = Ub * 2
-print("Total Utilization of the system (U):", U)
-print('Average Utilization of the system:', Ub)
+    # Average Queue Length
+    N = rho / (1 - rho)
+    Nq = N - U
+    print("Average queue length: ", Nq)
 
-p2 = 2 * (1 - rho) / (1 + rho) * (rho ** 2)
-print("Probability of having exactly 2 jobs in the system:", p2)
+    # Average Response Time
+    R = D / (1 - rho)
+    print("Average response time (R):", R)
 
-pi = np.zeros(5)
-pNlti = np.zeros(5)
+    # Probability that Response Time > 2
+    pRtg2 = np.exp(-2 / R)
+    print("Probability that the response time is greater than 2:", pRtg2)
 
-pi[0] = (1 - rho) / (1 + rho)
-pNlti[0] = pi[0]
+    # 95th Percentile
+    p95 = -R * np.log(0.05)
+    print("Percentile 95th:", p95)
+    print("")
 
-for i in range(1, 5):
-    pi[i] = 2 * pi[0] * (rho ** i)
-    pNlti[i] = pNlti[i - 1] + pi[i]
 
-print("Probability of having exactly 2 jobs in the system:", pi[2])
-print("Probability of having less than 5 jobs in the system:", pNlti)
+def second_configuration(lam):
+    print("Second Configuration")
+    rho = lam * D / 2
 
-N = 2 * rho / (1 - rho ** 2)
-Nq = N - U
-print("Average queue length: ", Nq)
+    # Utilization
+    Ub = rho
+    U = Ub * 2
+    print("Total Utilization of the system (U):", U)
+    print('Average Utilization of the system:', Ub)
 
-R = D / (1 - rho ** 2)
-print("Average response time (R):", R)
+    # Probability Calculations
+    pi = np.zeros(5)
+    pNlti = np.zeros(5)
 
-print("")
-# Third Configuration M/M/c
-print("Third Configuration")
-lam = 4
-c = np.ceil(lam * D)
-rho = lam * D / c
+    pi[0] = (1 - rho) / (1 + rho)
+    pNlti[0] = pi[0]
 
-Ub = rho
-U = Ub * c
-print("Average Utilization of the system:", Ub)
-print('Total Utilization of the system (U):', U)
+    for i in range(1, 5):
+        pi[i] = 2 * pi[0] * (rho ** i)
+        pNlti[i] = pNlti[i - 1] + pi[i]
+
+    print("Probability of having exactly 2 jobs in the system:", pi[2])
+    print("Probability of having less than 5 jobs in the system:", pNlti[-1])
+
+    # Average Queue Length
+    N = 2 * rho / (1 - rho ** 2)
+    Nq = N - U
+    print("Average queue length: ", Nq)
+
+    # Average Response Time
+    R = D / (1 - rho ** 2)
+    print("Average response time (R):", R)
+    print("")
+
+
+def third_configuration(lam):
+    print("Third Configuration")
+    c = int(np.ceil(lam * D))
+    rho, p0 = calculate_probabilities(lam, c)
+
+    # Utilization
+    Ub = rho
+    U = Ub * c
+    print('Total Utilization of the system (U):', U)
+    print("Average Utilization of the system:", Ub)
+
+    # Probability Calculations
+    pi = np.zeros(5)
+    pNlti = np.zeros(5)
+
+    pi[0] = p0
+    pNlti[0] = pi[0]
+
+    for n in range(1, 5):
+        if n < c:
+            pi[n] = pi[n - 1] * (c * rho) / n
+        else:
+            pi[n] = pi[n - 1] * rho
+        pNlti[n] = pNlti[n - 1] + pi[n]
+
+    print("Probability of having exactly 2 jobs in the system:", pi[2])
+    print("Probability of having less than 5 jobs in the system:", pNlti[-1])
+
+    # Average Queue Length
+    NSum = sum([(c * rho) ** i / math.factorial(i) for i in range(c)])
+    N = c * rho + (rho / (1 - rho)) / (1 + (1 - rho) * (math.factorial(c) / ((c * rho) ** c)) * NSum)
+    Nq = N - U
+    print("Average queue length: ", Nq)
+
+    # Average Response Time
+    R = N / lam
+    print("Average response time (R):", R)
+    print("")
+
+
+def final_configuration(lam):
+    print("Final Configuration")
+    rho = lam * D
+
+    # Utilization
+    U = rho
+    print("Utilization of the system (U):", U)
+
+    # Probability Calculations
+    pi = np.zeros(5)
+    pNlti = np.zeros(5)
+
+    pi[0] = np.exp(-rho)
+    pNlti[0] = pi[0]
+
+    for i in range(1, 5):
+        pi[i] = pi[i - 1] * rho / i
+        pNlti[i] = pNlti[i - 1] + pi[i]
+
+    print("Probability of having exactly 2 jobs in the system:", pi[2])
+    print("Probability of having less than 5 jobs in the system:", pNlti[-1])
+
+    # Average Response Time
+    N = U
+    R = N / lam
+    print("Average response time (R):", R)
+
+
+# Function Call
+initial_configuration(1 / 2)
+second_configuration(1)
+third_configuration(4)
+final_configuration(10)
